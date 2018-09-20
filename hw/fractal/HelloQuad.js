2ecb284a0216cfe6ba72aaa0602959ec8aca2d63
Daniil <danillkov2007@yandex.ru>
2018-09-20 16:10:23 +0300

    stable unscrolled modelbort

--- hw/fractal/HelloQuad.js
+++ hw/fractal/HelloQuad.js
@@ -2,19 +2,50 @@
 // Vertex shader program
 var VSHADER_SOURCE =
   'attribute vec4 a_Position;\n' +
+  'varying vec4 fragment_position;\n' +
   'void main() {\n' +
-  '  gl_Position = a_Position;\n' +
+  '  gl_Position = fragment_position = a_Position;\n' +
   '}\n';
 
 // Fragment shader program
 var FSHADER_SOURCE =
   'precision mediump float;\n' +
-  'void main() {\n' +
-  '  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n' +
+  'varying vec4 fragment_position;\n' +
+
+  'vec3 mandelbrot(vec2 c);\n' +
+  'float modulus(vec2 v);\n' +
+  'vec2 complex_product(vec2 a, vec2 b);\n' +
+  'vec3 jet(float t);\n' +
+
+  'vec3 mandelbrot(vec2 c) {\n' +
+  '  c.x = c.x - 0.5;\n' +
+  '  const int max_iteration = 5000;\n' +
+  '  vec2 z = c;\n' +
+  '  for (int i = 0; i < max_iteration; ++i) {\n' +
+  '    z = complex_product(z, z) + c;\n' +
+  '    if (modulus(z) >= 4.0) {\n' +
+  '      float dist_from_set = float(i) / float(max_iteration) * 2.0 - 0.05;\n' +
+  '      return jet(dist_from_set);\n' +
+  '    }\n' + 
+  '  }\n' +
+  '  return vec3(0);\n' +
   '}\n' +
-  'vec3 jet(float t) \n'
-  '{\n' + 
-  '  return clamp(vec3(1.5) - abs(4.0 * vec3(t) + vec3(-3, -2, -1)), vec3(0), vec3(1));\n' + 
+
+  'vec3 jet(float t){\n' +
+  '  return clamp(vec3(1.5) - abs(4.0*vec3(t) + vec3(-3,-2,-1)), vec3(0), vec3(1));\n' +
+  '}\n' +
+  'float modulus(vec2 v) {\n' +
+  '  vec2 w = v*v;\n' +
+  '  return w.x + w.y;\n' +
+  '}\n' +
+  'vec2 complex_product(vec2 a, vec2 b) {\n' +
+  '  return vec2(a.x*b.x - a.y*b.y, a.x*b.y + a.y*b.x);\n' +
+  '}\n' +
+
+  'void main() {\n' +
+  '  vec2 point = fragment_position.xy;\n' +
+  '  vec3 c = mandelbrot(point);\n' +
+  '  gl_FragColor = vec4(c.x, c.y, c.z, 1.0);\n' +
   '}\n';
 
 
